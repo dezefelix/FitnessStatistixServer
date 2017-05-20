6c0ae83ba3c2857bf0f3118082d32f4282b18765
Felix Boons <felixboons@hotmail.com>
2017-05-20 17:44:25 +0200

    database-config+connector-made

---
+++ database/db_connector.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Felix on 20-5-2017.
+ */
+
+var config = require('./config.json');
+var mysql = require('mysql');
+
+var connector = mysql.createPool({
+    host : config.dbHost,
+    user : config.dbUser,
+    password : config.dbPassword,
+    database : config.dbDatabase
+});
+
+module.exports = connector;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 /**
  * Created by Felix on 20-5-2017.
  */
+
+var express = require('express');
+var connector = require('./database/db_connector');
+var app = express();
