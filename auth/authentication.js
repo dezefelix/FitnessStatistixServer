67c17b43d5e8ba209e5961784c08397c93d91c18
Felix Boons <felixboons@hotmail.com>
2017-08-11 11:45:01 +0200

    user endpoints added. register almost done

---
+++ auth/authentication.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Felix on 11/08/17.
+ */
+var settings = require('../config.json');
+const moment = require('moment');
+const jwt = require('jwt-simple');
+
+//
+// Encode (van email naar token)
+//
+function encodeToken(username) {
+    const playload = {
+        exp: moment().add(10, 'days').unix(),
+        iat: moment().unix(),
+        sub: username
+    };
+    return jwt.encode(playload, settings.secretKey);
+}
+
+//
+// Decode (van token naar username)
+//
+function decodeToken(token, cb) {
+
+    try {
+        const payload = jwt.decode(token, settings.secretKey);
+
+        // Check if the token has expired. To do: Trigger issue in db ..
+        const now = moment().unix();
+
+        // Check if the token has expired
+        if (now > payload.exp) {
+            console.log('Token has expired.');
+        }
+
+        // Return
+        cb(null, payload);
+
+    } catch(err) {
+        cb(err, null);
+    }
+}
+
+module.exports = {
+    encodeToken,
+    decodeToken
+};
--- routes/api_user.js
+++ routes/api_user.js
@@ -39,4 +39,74 @@ router.get('/:username?', function (req, res) {
     })
 });
 
+//every endpoint below, except for /login, needs JWT authorization
+router.all(new RegExp("[^(\/login)]"), function (req, res, next) {
+
+    console.log("VALIDATING TOKEN");
+
+    var token = (req.header('Auth')) || '';
+
+    auth.decodeToken(token, function (err, payload) {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
+        } else {
+            next();
+        }
+    });
+});
+
+//register a user
+router.post('/register', function (req, res) {
+
+    var firstName = req.body.firstName || '';
+    var lastName = req.body.lastName || '';
+    var email = req.body.email || '';
+    var password = req.body.password || '';
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, null, function (err, hash) {
+            password = hash;
+
+            pool.getConnection(function (err, con) {
+                con.query('INSERT INTO customer (first_name, last_name, email, password, active, create_date, last_update) VALUES ' +
+                    '("' + firstName + '", "' + lastName + '", "' + email + '", "' + password + '", 0, now(), now());', function (error) {
+                    con.release();
+                    if (error) {
+                        res.status(400).json({"error": "registration failed"});
+                    } else {
+                        res.status(200).json({"registration": "success"});
+                    }
+                });
+            });
+        });
+    });
+});
+
+//login user and return a JWT
+router.post('/login', function (req, res) {
+    var email = req.body.email || '';
+    var password = req.body.password || '';
+
+    pool.getConnection(function (err, con) {
+        con.query("SELECT * FROM customer WHERE email = '" + email + "';", function (err, rows) {
+            con.release();
+            if (err) {
+                throw err;
+            }
+            var hashPass = rows[0].password;
+            bcrypt.compare(password, hashPass, function (err, response) {
+                if (response) {
+                    res.status(200).json({
+                        "token": auth.encodeToken(email),
+                        "customerID": rows[0].customer_id
+                    });
+                } else {
+                    res.status(401).json({"error": "Invalid credentials"});
+                }
+            });
+        });
+    });
+});
+
 module.exports = router;
